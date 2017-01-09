a443fb710376c2f032949366702e3520d44cabfe
Kory33 <konryoitdv@gmail.com>
2017-01-09 15:13:08 +0100

    add implementation to core

--- src/main/java/com/github/kory33/bukkitserializationinterface/BukkitSerializationInterfacePlugin.java
+++ src/main/java/com/github/kory33/bukkitserializationinterface/BukkitSerializationInterfacePlugin.java
@@ -1,17 +1,17 @@
 package com.github.kory33.bukkitserializationinterface;
 
+import com.github.kory33.bukkitserializationinterface.command.SerializationInterfaceCommandExecutor;
 import com.github.kory33.updatenotificationplugin.bukkit.github.GithubUpdateNotifyPlugin;
 
 public class BukkitSerializationInterfacePlugin extends GithubUpdateNotifyPlugin{
     @Override
     public void onEnable() {
-        // TODO 自動生成されたメソッド・スタブ
         super.onEnable();
+        this.getCommand("serialinterface").setExecutor(new SerializationInterfaceCommandExecutor());
     }
     
     @Override
     public void onDisable() {
-        // TODO 自動生成されたメソッド・スタブ
         super.onDisable();
     }
     
