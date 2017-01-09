8dd377decf913156a7a9af9fda12f9347d2828f3
Kory33 <konryoitdv@gmail.com>
2017-01-09 14:47:39 +0100

    initialized the project

---
+++ src/main/java/com/github/kory33/bukkitserializationinterface/BukkitSerializationInterfacePlugin.java
@@ -0,0 +1,12 @@
+package com.github.kory33.bukkitserializationinterface;
+
+import com.github.kory33.updatenotificationplugin.bukkit.github.GithubUpdateNotifyPlugin;
+
+public class BukkitSerializationInterfacePlugin extends GithubUpdateNotifyPlugin{
+
+    @Override
+    public String getGithubRepository() {
+        // TODO 自動生成されたメソッド・スタブ
+        return null;
+    }
+}
