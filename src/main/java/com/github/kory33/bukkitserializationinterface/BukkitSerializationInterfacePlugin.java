959bde11511602ef5f5dc7a5c97611408706ed23
Kory33 <konryoitdv@gmail.com>
2017-01-09 14:54:35 +0100

    implement core class

--- src/main/java/com/github/kory33/bukkitserializationinterface/BukkitSerializationInterfacePlugin.java
+++ src/main/java/com/github/kory33/bukkitserializationinterface/BukkitSerializationInterfacePlugin.java
@@ -3,7 +3,18 @@ package com.github.kory33.bukkitserializationinterface;
 import com.github.kory33.updatenotificationplugin.bukkit.github.GithubUpdateNotifyPlugin;
 
 public class BukkitSerializationInterfacePlugin extends GithubUpdateNotifyPlugin{
-
+    @Override
+    public void onEnable() {
+        // TODO 自動生成されたメソッド・スタブ
+        super.onEnable();
+    }
+    
+    @Override
+    public void onDisable() {
+        // TODO 自動生成されたメソッド・スタブ
+        super.onDisable();
+    }
+    
     @Override
     public String getGithubRepository() {
         return "Kory33/BukkitSerializationInterface";
